596d94b086a8fc89229c75f17508462216e197ec
Yiranubari <yiranubari4@gmail.com>
2026-03-02 05:27:59 +0100

    remove /hello route handler from server.js

--- src/server.js
+++ src/server.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const app = express();
 
-app.get("/hello", (req, res) => {
-  res.json({ message: "Hello, World!" }); //
-});
-
 const PORT = 5001;
 const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
