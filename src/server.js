639cdffbdab6a18774f5a4d87adac11da580e7da
Yiranubari <yiranubari4@gmail.com>
2026-03-02 05:37:41 +0100

    add movieRoutes import and set up API route for movies

--- src/server.js
+++ src/server.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 
+// Import Routes
+import movieRoutes from "./routes/movieRoutes.js";
+
+// API Routes
+app.use("/movies", movieRoutes);
+
 const PORT = 5001;
 const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
