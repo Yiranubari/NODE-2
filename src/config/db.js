c33e4194d4ab24890217aea542745f83085e2488
Yiranubari <yiranubari4@gmail.com>
2026-03-03 05:56:17 +0100

    fix: integrate Prisma adapter for PostgreSQL and update dependencies

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,14 @@
 import pkg from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 const { PrismaClient, Prisma } = pkg;
 
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+
 const prisma = new PrismaClient({
+  adapter,
   log:
     process.env.NODE_ENV === "development"
       ? ["query", "error", "warn"]
