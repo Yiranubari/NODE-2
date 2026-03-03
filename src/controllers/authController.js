1b394922b0dff6b9e3e5ce7abb96202aba3d2f35
Yiranubari <yiranubari4@gmail.com>
2026-03-03 06:13:36 +0100

    fix: implement user existence check in register endpoint

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,18 @@
+import { PrismaClient } from "@prisma/client";
 const register = async (req, res) => {
   const body = req.body;
-  res.json(body);
+  const { name, email, password } = req.body;
+
+  // check if user already exists
+  const userExists = await prisma.user.findUnique({
+    where: { email },
+  });
+
+  if (userExists) {
+    return res
+      .status(400)
+      .json({ message: "User already exists with this email" });
+  }
 };
 
 export { register };
