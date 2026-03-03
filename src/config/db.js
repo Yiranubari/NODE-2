e9fb3427283ae047f43574fe73a9aaa8740954a0
Yiranubari <yiranubari4@gmail.com>
2026-03-03 05:44:42 +0100

    fix: correct instantiation of prismaClient in db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 import { prismaClient } from "@prisma/client";
 import { Prisma } from "@prisma/client/extension";
 
-const prisma = new PrismaClient({
+const prisma = new prismaClient({
   log:
     process.env.NODE_ENV === "development"
       ? ["query", "error", "warn"]
