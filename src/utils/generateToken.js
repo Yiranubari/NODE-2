1cfc475482373482eeac2b7aef2f4d1246386cd6
Yiranubari <yiranubari4@gmail.com>
2026-03-03 15:20:11 +0100

    feat: update generateToken function to accept response object for cookie handling

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 
-export const generateToken = (userId) => {
+export const generateToken = (userId, res) => {
   const payload = { id: userId };
   const token = jwt.sign(payload, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
