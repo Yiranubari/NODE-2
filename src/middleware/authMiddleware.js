76ab607e063c068ccb17734362b8e4e9267d6322
Yiranubari <yiranubari4@gmail.com>
2026-03-04 08:31:03 +0100

    feat: enhance authMiddleware to handle token retrieval and verification

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -12,5 +12,14 @@ export const authMiddleware = async (req, res, next) => {
     req.headers.authorization.startsWith("Bearer")
   ) {
     token = req.headers.authorization.split(" ")[1];
+  } else if (req.cookies.jwt) {
+    token = req.cookies.jwt;
   }
+  if (!token) {
+    return res.status(401).json({ message: "Not authorized, no token" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (err) {}
 };
