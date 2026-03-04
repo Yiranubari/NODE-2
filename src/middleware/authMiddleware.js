039796ca9037fa7916cf97fe10b5173d1534dda8
Yiranubari <yiranubari4@gmail.com>
2026-03-04 08:27:06 +0100

    feat: update authMiddleware to accept request, response, and next parameters

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -2,6 +2,15 @@ import jwt from "jsonwebtoken";
 import { prisma } from "../config/db.js";
 
 // Read token from the request
-export const authMiddleware = async () => {
+export const authMiddleware = async (req, res, next) => {
   console.log("Auth Middleware Invoked");
+
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  }
 };
